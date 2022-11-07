d674c08367567b559975e0f9da0a1d750b7c76c5
luizlcezario <luizlcezario@gmail.com>
2022-11-07 11:18:15 -0300

    ifx

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: llima-ce <llima-ce@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:48:29 by llima-ce          #+#    #+#             */
-/*   Updated: 2022/11/03 17:26:51 by llima-ce         ###   ########.fr       */
+/*   Updated: 2022/11/07 11:17:08 by llima-ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	key_hook(int keycode, t_vars *vars)
 
 void print_map(t_vars *vars)
 {
-	
+	(void) vars;
 }
 
 int	main(int argc,char **argv)
@@ -32,7 +32,7 @@ int	main(int argc,char **argv)
 	if(argc == 1) {
 		vars.mlx = mlx_init();
 		vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-		print_map(vars);
+		print_map(&vars);
 		mlx_key_hook(vars.win, key_hook, &vars);
 		mlx_loop(vars.mlx);
 	}
